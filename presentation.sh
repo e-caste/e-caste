84714f451915a4266426b541dd8f18e6414f5c25
e-caste-mbp <en.cas.dev@icloud.com>
2021-01-04 00:00:03 +0100

    add presentation.sh

---
+++ presentation.sh
@@ -0,0 +1,39 @@
+#!/bin/zsh
+
+PYTHONS="🐍🐍🐍🐍🐍🐍🐍🐍🐍🐍🐍🐍🐍🐍🐍🐍🐍🐍🐍🐍🐍🐍🐍🐍🐍🐍"
+SLEEP_TIME=2  # seconds
+
+autoload -U colors &> /dev/null
+colors &> /dev/null
+
+figlet "Welcome to e-caste's GitHub profile!" | lolcat
+echo
+sleep $SLEEP_TIME
+
+echo $PYTHONS
+figlet "I like Python"
+echo $PYTHONS
+echo
+sleep $SLEEP_TIME
+
+# $bold_color$bg[green]
+echo "$fg[green]Did you know that Python got its name from Monty Python?${reset_color}"
+sleep $SLEEP_TIME
+
+# ~/imgcat does not work in asciiterm since it's iTerm2 only
+catimg -r 2 -w 90 ~/Downloads/spin.jpeg
+echo "$fg[red]Nobody expects the Spanish Inquisition! Especially when programming!${reset_color}"
+echo
+sleep $SLEEP_TIME
+
+figlet "Check out my projects below!"
+echo
+sleep $SLEEP_TIME
+
+###
+#figlet Welcome to e-caste\'s GitHub profile! | lolcat
+#echo $PYTHONS && figlet "I like Python" && echo $PYTHONS
+#echo "$bold_color$bg[green]$fg[red]Did you know that Python got its name from Monty Python?${reset_color}"
+#catimg ~/Downloads/spin.jpeg && echo Nobody expects the Spanish Inquisition! Especially when programming!
+#figlet Check out my projects below!
+###
