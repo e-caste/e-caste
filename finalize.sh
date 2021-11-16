35f1a2d2bdcf31b7ecfa5376e5faa4c82b558ec7
e-caste-mbp <git@caste.dev>
2021-11-16 17:42:28 +0100

    finalize to gif

--- finalize.sh
+++ finalize.sh
@@ -1,4 +1,10 @@
 #!/bin/zsh
-svg-term --in term.cast --out _term.svg --window
-svgembed -i _term.svg -o term.svg -f ~/Library/Fonts/SF-Mono-Powerline-Regular.otf --overwrite
-rm _term.svg
+
+# SVG output
+#svg-term --in term.cast --out _term.svg --window
+#svgembed -i _term.svg -o term.svg -f ~/Library/Fonts/SF-Mono-Powerline-Regular.otf --overwrite
+#rm _term.svg
+
+# GIF output
+GIFSICLE_OPTS="-k 256 -O3" asciicast2gif -w 80 -h 20 term.cast term.gif
+
